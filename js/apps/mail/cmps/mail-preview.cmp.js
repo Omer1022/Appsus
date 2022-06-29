9e78d6d490af2965636fc40d391ee8cf8058d8f9
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-06-29 16:15:42 +0300

    add trash icon

--- js/apps/mail/cmps/mail-preview.cmp.js
+++ js/apps/mail/cmps/mail-preview.cmp.js
@@ -7,13 +7,18 @@ export default {
         <router-link :to="'/email/'+email.id">{{email.from}}</router-link>
         <router-link :to="'/email/'+email.id">{{email.subject}} - {{email.body}}</router-link>
         <li>{{email.sentAt}}</li>
-        <button style="background: url(img/delete.png)" @click="remove(email.id)"></button>
+        <button class="remove-mail" style="background: url(img/delete.png)" 
+        @click="remove(email.id)"></button>
 </div>
 </ul>
       `,
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    remove(emailId) {
+      this.$emit("removed", emailId);
+    },
+  },
   computed: {},
 };
