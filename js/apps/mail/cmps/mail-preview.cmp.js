1c4d9ae11299f328ad26eb79446d9607d7f90342
Omer Dahan <92370133+Omer1022@users.noreply.github.com>
2022-06-30 15:42:58 +0300

    bold text

--- js/apps/mail/cmps/mail-preview.cmp.js
+++ js/apps/mail/cmps/mail-preview.cmp.js
@@ -2,22 +2,22 @@ export default {
   props: ["email"],
   template: `
       <ul>
-    <div class="mail-preview">
+    <div class="mail-preview" style="{ font-weight: bold }">
         <li><input type="checkbox" /></li>
         <li>⭐ </li>
         <router-link :to="'/email/'+email.id">
-        <strong>
         <li>{{email.from}}</li>
         <li>{{email.subject}} -</li>
         <li class="email-body">{{email.body}}</li>
         <li>{{email.sentAt}}</li>
-    </strong>
     </router-link>
   </div>
 </ul>
       `,
   data() {
-    return {};
+    return {
+      isRead: false,
+    };
   },
   methods: {},
   computed: {},
