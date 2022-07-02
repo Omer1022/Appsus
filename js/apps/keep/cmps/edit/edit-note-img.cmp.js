3138cc50e4c93847ec0f8392f7b245a867a088c0
eran_pascal <eranpascal@gmail.com>
2022-07-02 13:33:31 +0300

    edit and add dynamicly - worksgit add .git add ., need to fix the linthrogh to update across the modal

--- js/apps/keep/cmps/edit/edit-note-img.cmp.js
+++ js/apps/keep/cmps/edit/edit-note-img.cmp.js
@@ -1,10 +1,21 @@
 export default {
+    props:["note"],
  template: `
+ 
+ <img :src="noteToEdit.info.url" alt="">
+ <input  v-model="noteToEdit.info.title" type="text" placeHolder="enter a title...">
+ <input  v-model="noteToEdit.info.url" type="text" placeHolder="enter a url...">
+ <button class="close-modal" @click="$emit('close', note)">Close</button>
 `,
 data() {
-return {};
+return {
+    noteToEdit:null
+};
 },
-created() {},
+created() {
+    this.noteToEdit = this.$props.note
+    console.log( this.noteToEdit.info.url)
+},
 methods: {},
 computed: {},
 unmounted() {},
--- js/apps/keep/cmps/edit/edit-note-todos.cmp.js
+++ js/apps/keep/cmps/edit/edit-note-todos.cmp.js
@@ -1,4 +1,7 @@
 import todoList from "../previews/todo-list.cmp.js";
+import { todoService } from "../../services/todo-service.js";
+import { utilService } from "../../services/util-service.js";
+
 export default {
     props:["note"],
     template: `
@@ -6,18 +9,27 @@ export default {
  <!-- <pre>{{note}}</pre> -->
  <input type="text" v-model="note.info.label" >
  <todo-list :todos="note.info.todos"/>
+ <li><input v-model="newTask" type="text" placeholder="add task.." @click.stop><button @click="addTask">add</button></li>
  <!-- <textarea name="" id="" cols="30" rows="10"  v-model="note.info.todos"></textarea> -->
   <button class="close-modal" @click="$emit('close', note)">Close</button>
 `,
     data() {
         return {
             note:null,
+            newTask: null
         };
     },
     created() { 
         this.note = this.$props.note
     },
-    methods: {},
+    methods: {
+        addTask(){
+            const newTodo = todoService.getEmptyTodo()
+            newTodo.id = utilService.makeId()
+            newTodo.txt = this.newTask
+            this.note.info.todos.push(newTodo)
+        }
+    },
     computed: {},
     unmounted() { },
     components:{
